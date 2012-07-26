09e09aa61f6f85758354aaaaa06f008f7e18408d
John Murray <jmurrayufo@gmail.com>
2012-07-25 19:51:46 -0600

    Removed some uneeded print statements.

--- Src/main.cpp
+++ Src/main.cpp
@@ -7,9 +7,6 @@
 //==============================================================================
 int main(int argc, char* argv[]) {
     CApp theApp;
-    printf("%s:%d\n",__FILE__,__LINE__);
-    for(int i=0;i<argc;i++)
-        printf("%s\n",argv[i]);
     return theApp.OnExecute();
 }
 
