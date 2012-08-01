0cf03fdf524b113e8e99ca72a1a9d6f4fa824f2c
John Murray <jmurrayufo@gmail.com>
2012-07-31 22:21:10 -0600

    Basic functions put in place, they don't compile yet or DO anything. Work for tomorrow!

--- Src/entities/CTexture.cpp
+++ Src/entities/CTexture.cpp
@@ -9,3 +9,48 @@ CTexture::~CTexture()
 {
     //Void
 }
+
+CTexture* CTexture::InitTexture(const char* File)
+{
+    
+}
+
+bool CTexture::DeleteTexture(void)
+{
+    
+}
+
+int CTexture::CheckTexture(const char* File)
+{
+    
+}
+
+unsigned int CTexture::GetX(void)
+{
+    
+}
+
+unsigned int CTexture::GetY(void)
+{
+    
+}
+
+int CTexture::GetXY(int& x,int& y)
+{
+    
+}
+
+bool CTexture::SetX(int x)
+{
+    
+}
+
+bool CTexture::SetY(int y)
+{
+    
+}
+
+bool CTexture::SetXY(int x,int y)
+{
+    
+}
