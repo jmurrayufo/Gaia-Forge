1eada0e0db925e9858ed734c9f265a10cd52fd1e
Zanzabar <mongo988@myfairpoint.net>
2012-08-03 00:06:06 -0400

    updated with function constructor and varialble prototypes. will comment later its way too late and i have work tomarrow ok now im rambling and ill stop.

--- Src/system/map/CTile.h
+++ Src/system/map/CTile.h
@@ -1,22 +1,27 @@
 #ifndef _CTILE_H_
-    #define _CTILE_H_
+#define _CTILE_H_
 
 #include "..\..\Define.h"
 
 enum {
-   TILE_TYPE_NONE = 0,
+    TILE_TYPE_NONE = 0,
 
-   TILE_TYPE_NORMAL,
-   TILE_TYPE_BLOCK
+    TILE_TYPE_NORMAL,
+    TILE_TYPE_BLOCK,
 };
 
 class CTile {
-   public:
-      int     TileID;
-      int     TypeID;
+public:
 
-   public:
-      CTile();
+    short int tileID;
+
+public:
+    CTile();
+    CTile(short TileID, bool Collideable = 1);
+    bool Collision();
+protected:
+
+    bool collideable;
 };
 
 #endif
