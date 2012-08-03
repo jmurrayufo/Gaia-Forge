e179e59a182c359f01e5c3e886ad7dcd5bc2ffd0
John Murray <jmurrayufo@gmail.com>
2012-08-02 22:59:41 -0600

    Remove the test system in main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,28 +3,10 @@
 #include <stdio.h>
 #include <SDL.h>
 #include "capp/CApp.h"
-#include "system/graphics/CAnimation.h"
 
 //Johns Test comment
 //==============================================================================
 int main(int argc, char* argv[]) {
-
-    CAnimation test;
-    int x,y;
-
-    test.SetFrameRate(1000);
-    test.tiles=6;
-    test.cols=2;
-
-    while(1)
-    {
-        test.OnAnimate();
-        test.GetCurrentFrame(x,y);
-        std::cout << "Tick:" << SDL_GetTicks() << std::endl;
-        std::cout << "x:" << x << " y:" << y << std::endl << std::endl;
-        SDL_Delay(500);
-    }
-
     CApp theApp;
     fprintf(stdout,"Booted main()\n   %s:%d\n",__FILE__,__LINE__);
     return theApp.OnExecute();
