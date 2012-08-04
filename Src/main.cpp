ca9af0f723a4b56716abb11a3f1bb3dfc7dce127
John Murray <jmurrayufo@gmail.com>
2012-08-04 00:43:45 -0600

    Some handy test code for the JSON parser

--- Src/main.cpp
+++ Src/main.cpp
@@ -13,12 +13,21 @@ using namespace std;
 //==============================================================================
 int main(int argc, char* argv[]) {
     using boost::property_tree::ptree;
+    using namespace boost::property_tree::json_parser;
     ptree pt;
 
-    boost::property_tree::json_parser::read_json((std::string)"test.json",pt);
+    read_json("test.json", pt);
 
     cout << pt.get("test","What Happened?") << endl; 
-    cout << pt.get("test1","What Happened?") << endl;
+    cout << pt.get("test1.item1","What Happened?") << endl;
+    cout << pt.get("test1.item1","What Happened?") << endl;
+    pt.add("test1.item2",3.14f);
+    pt.add("1.2.3.4.5",6);
+    pt.add("test1.item2",3.14f);
+
+    write_json("test2.json", pt);
+
+
 
     CApp theApp;
     fprintf(stdout,"Booted main()\n   %s:%d\n",__FILE__,__LINE__);
