b325bd2fa875276aad00b8a11b6d72293ed69231
John Murray <jmurrayufo@gmail.com>
2012-08-04 11:54:07 -0600

    Clean up of main.cpp for the merge

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,8 +3,6 @@
 #include <string>
 #include <stdio.h>
 #include <SDL.h>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
 #include "capp/CApp.h"
 
 using namespace std;
@@ -12,23 +10,6 @@ using namespace std;
 //Johns Test comment
 //==============================================================================
 int main(int argc, char* argv[]) {
-    using boost::property_tree::ptree;
-    using namespace boost::property_tree::json_parser;
-    ptree pt;
-
-    read_json("test.json", pt);
-
-    cout << pt.get("test","What Happened?") << endl; 
-    cout << pt.get("test1.item1","What Happened?") << endl;
-    cout << pt.get("test1.item1","What Happened?") << endl;
-    pt.add("test1.item2",3.14f);
-    pt.add("1.2.3.4.5",6);
-    pt.add("test1.item2",3.14f);
-
-    write_json("test2.json", pt);
-
-
-
     CApp theApp;
     fprintf(stdout,"Booted main()\n   %s:%d\n",__FILE__,__LINE__);
     return theApp.OnExecute();
