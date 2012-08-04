03ea04036c2af2d094e985d79c0b818b2f71367c
John Murray <jmurrayufo@gmail.com>
2012-08-04 00:20:45 -0600

    Working boost::PropertyTree code! WE CAN PARSE FILES NOW!!!!

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,14 +1,25 @@
 
 #include <ostream>
+#include <string>
 #include <stdio.h>
 #include <SDL.h>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include "capp/CApp.h"
 
+using namespace std;
+
 //Johns Test comment
 //==============================================================================
 int main(int argc, char* argv[]) {
+    using boost::property_tree::ptree;
+    ptree pt;
+
+    boost::property_tree::json_parser::read_json((std::string)"test.json",pt);
+
+    cout << pt.get("test","What Happened?") << endl; 
+    cout << pt.get("test1","What Happened?") << endl;
+
     CApp theApp;
     fprintf(stdout,"Booted main()\n   %s:%d\n",__FILE__,__LINE__);
     return theApp.OnExecute();
