122b3e21f27d06e782c850c0b6042403815b1120
John Murray <jmurrayufo@gmail.com>
2012-08-05 22:32:57 -0600

    Adding some test code to render the perlin noise function.

--- Src/capp/CApp_OnRender.cpp
+++ Src/capp/CApp_OnRender.cpp
@@ -12,10 +12,22 @@ void CApp::OnRender()
             continue;
 
         CEntity::EntityList[i]->OnRenderGL();
-    }    
-    
+    }
+
+    // Lets do a Sin wave test!
+    Noise x;
+    x.Period = 100;
+    x.Amplitude = 80;
+    x.Octaves = 6;
+    x.Persistance = 2;
+    x.InterpType = NOISE_INTERP_CUBIC;
+    for (int i = 0; i < WWIDTH; ++i)
+    {
+        GLDebug::DrawPoint(i+0.01,x.Perlin1d(i + SDL_GetTicks()/20.f) + WHEIGHT/2);
+    }
+
     SDL_GL_SwapBuffers();
-    //SDL_Delay(1000/60);
+    SDL_Delay(1000/60);
 }
 
 //==============================================================================
