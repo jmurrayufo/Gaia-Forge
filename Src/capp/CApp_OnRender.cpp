517f815df191cae1845e3ef35c8b297f3d99d0d8
John Murray <jmurrayufo@gmail.com>
2012-08-05 18:26:29 -0600

    We don't need to bind the texture, the entities do that now.

--- Src/capp/CApp_OnRender.cpp
+++ Src/capp/CApp_OnRender.cpp
@@ -6,9 +6,6 @@ void CApp::OnRender()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Bind the texture object
-    glBindTexture(GL_TEXTURE_2D,(&texture)[0]);
-
     for(unsigned int i = 0;i < CEntity::EntityList.size();i++) {
 
         if(!CEntity::EntityList[i]) 
