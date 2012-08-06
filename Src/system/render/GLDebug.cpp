dc6f00dfd138ee81c7588f2f34bf02b3b4d382b3
John Murray <jmurrayufo@gmail.com>
2012-08-05 18:26:48 -0600

    Added the DrawPixel class, and did some cleanup.

--- Src/system/render/GLDebug.cpp
+++ Src/system/render/GLDebug.cpp
@@ -7,24 +7,49 @@ GLfloat GLDebug::colors[4];
 
 //==============================================================================
 
+void GLDebug::Init()
+{
+    lineWidth=1;
 
+    for (int i = 0; i < 4; ++i)
+    {
+        colors[i]=1;
+    }
 
+}
 
 void GLDebug::DrawLine( float x1 , float y1 , float x2 , float y2)
 {
-    colors[0]=0;
-    colors[1]=0;
-    colors[2]=1;
-    colors[3]=1;
+    GLDebug::DrawLine(x1,y1,x2, y2,colors);
+}
+
+
+void GLDebug::DrawLine(float x1,float y1,float x2, float y2,GLfloat *funcColors)
+{
     glDisable(GL_TEXTURE_2D);
     glLineWidth(lineWidth);
-    glColor4f(colors[0],colors[1],colors[2],colors[3]);
-    glBlendFunc(GL_DST_ALPHA,GL_ONE_MINUS_DST_ALPHA);
+    glColor4f(funcColors[0],funcColors[1],funcColors[2],funcColors[3]);
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
     glBegin(GL_LINES);
         glVertex2f(x1,y1);
         glVertex2f(x2,y2);
     glEnd();
-
 }
 
+void GLDebug::DrawPoint(float x1,float y1)
+{
+    GLDebug::DrawPoint(x1,y1,colors);
+}
+
+ 
+void GLDebug::DrawPoint(float x1,float y1,GLfloat* funcColors)
+{
+    glDisable(GL_TEXTURE_2D);
+    glLineWidth(lineWidth);
+    glColor4f(funcColors[0],funcColors[1],funcColors[2],funcColors[3]);
+    glBlendFunc(GL_DST_ALPHA,GL_ONE_MINUS_DST_ALPHA);
+    glBegin(GL_POINTS);
+        glVertex2f(x1, y1);
+    glEnd();
+}
 //==============================================================================
--- Src/system/render/GLDebug.h
+++ Src/system/render/GLDebug.h
@@ -11,7 +11,20 @@ public:
 
     static GLfloat colors[4];
 
+    static void Init(void);
+
+    //! Draw a line with the system colors
     static void DrawLine(float x1,float y1,float x2, float y2);
+
+    //! Draw a line with the given colors
+    static void DrawLine(float x1,float y1,float x2, float y2,GLfloat*);
+
+    //! Draw a point on the screen. Line Width matters here!
+    //! \note This will use the currently set colors
+    static void DrawPoint(float x1,float y1);
+
+    //! Draw a point on the screen with a specific color. 
+    static void DrawPoint(float x1,float y1,GLfloat*);
 
 };
 
