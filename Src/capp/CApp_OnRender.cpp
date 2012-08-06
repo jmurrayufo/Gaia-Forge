22fb7a3f83cfabc82b977516461b6663624c655f
John Murray <jmurrayufo@gmail.com>
2012-08-05 19:36:59 -0600

    Cleanout of the OnRender code

--- Src/capp/CApp_OnRender.cpp
+++ Src/capp/CApp_OnRender.cpp
@@ -13,14 +13,6 @@ void CApp::OnRender()
 
         CEntity::EntityList[i]->OnRenderGL();
     }    
-
-    for (int i = 0; i < WWIDTH; ++i)
-    {
-        float y=0;
-        y=sin((float)i/10.f+SDL_GetTicks()/1000.f) * 50.f + WHEIGHT / 2.f;
-        GLDebug::DrawPoint(i , y);
-    }
-
     
     SDL_GL_SwapBuffers();
     //SDL_Delay(1000/60);
