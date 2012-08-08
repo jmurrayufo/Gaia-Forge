00e205cdbebbc339786a896cb23d0802fe1eb85c
John Murray <jmurrayufo@gmail.com>
2012-08-07 21:27:39 -0600

    Move around of test code.

--- Src/capp/CApp_OnRender.cpp
+++ Src/capp/CApp_OnRender.cpp
@@ -17,25 +17,28 @@ void CApp::OnRender()
     // Lets do a Sin wave test!
     Noise x;
     x.Period = 100;
-    x.Amplitude = 80;
+    x.Amplitude = WHEIGHT/10;  
     x.Octaves = 6;
     x.Persistance = 2;
+    x.Seed = 0;
     GLDebug::lineWidth = 1;
-    x.InterpType = NOISE_INTERP_CUBIC;
+    x.InterpType = NOISE_INTERP_LINEAR;
     for (int i = 0; i < WWIDTH; ++i)
     {
-        //GLDebug::DrawPoint(i+0.5,x.Perlin1d(i + SDL_GetTicks()/20.f) + WHEIGHT/2 + SDL_GetTicks()/10000.f);
+        GLDebug::DrawPoint(i+0.5,x.Perlin1d(i + SDL_GetTicks()/20.f) + WHEIGHT/2);
+        /*
         GLDebug::DrawPoint(i+0.5,sin(i/10.0 + SDL_GetTicks()/1000.f)*50 + WHEIGHT/2 + 0.5);
         GLDebug::DrawLine(  i,
                             sin(i/10.0 + SDL_GetTicks()/1000.f)*50 + WHEIGHT/3 + 0.5,  
                             i+1,
                             sin(i/10.0 + SDL_GetTicks()/1000.f)*50 + WHEIGHT/3 + 0.5
                             );
+        */
     }
 
 
     SDL_GL_SwapBuffers();
-    SDL_Delay(1000/60);
+    //SDL_Delay(1000/60);
 }
 
 //==============================================================================
