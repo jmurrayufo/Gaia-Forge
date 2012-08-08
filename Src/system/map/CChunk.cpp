a2742e234bfcef0cd11e7e02b7fcc641a4736753
Zanzabar <mongo988@myfairpoint.net>
2012-08-08 00:14:02 -0400

    needs some fixing with the Null and another error johns gonna fix it (He said he would)

--- Src/system/map/CChunk.cpp
+++ Src/system/map/CChunk.cpp
@@ -1,7 +1,26 @@
 #include "CChunk.h"
 
-CChunk::CChunk() {
+CChunk::CChunk()
+{
+    for(int i = 0; i < CHUNK_X_DEM; ++i)
+    {
+        for(int j = 0; j < CHUNK_Y_DEM; ++i)
+        {
+            ChunkArray[i][j] = NULL;
+        }
+    }
 }
 
-CChunk::~CChunk() {
+
+CChunk::~CChunk() 
+{
+    for(int i = 0; i < CHUNK_X_DEM; ++i)
+    {
+        for(int j = 0; j < CHUNK_Y_DEM; ++i)
+        {
+            delete ChunkArray[i][j];
+        }
+
+    }
+    
 }
--- Src/system/map/CChunk.h
+++ Src/system/map/CChunk.h
@@ -1,5 +1,5 @@
 #ifndef _CCHUNK_H_
-    #define _CCHUNK_H_
+#define _CCHUNK_H_
 
 /*!
 \brief Maintain a given segment of map tiles. Deal with saving/loading files from the HDD. 
@@ -7,27 +7,41 @@ This class maintains a large segment of tile space in the game. It will load up,
     and save groups of tiles. 
 */
 #include "CTile.h"
+enum 
+{
+    //  Dimentions of chunk from top left corner to bottom right
+    CHUNK_X_DEM =1024,
+    CHUNK_Y_DEM =1024,
+}
+
+class CChunk 
+{
 
 
-class CChunk {
 
-    // TODO: What does X and Y represent? Lower/upper, left/right corner? This should NOT 
-    //      be the middle of the chunk as this would add lots more math to worry about. 
-
-    //! X location of chunk
-    unsigned int X;    
-
-    //! Y location of chunk
-    unsigned int Y;
 
     // TODO: Add a container for a large number of tiles. The current plan is to have 
     //      1024 x 1024 tiles in a chunk (1,048,576 tiles total). This might be too much 
     //      or not enough, we need to decide. Smaller chunks will result in more 
     //      processor overhead, but less memory footprint. 
+     
 
-    public:
-        CChunk();
-        ~CChunk();
+public:
+    //! X location of chunk
+    unsigned int X;
+    //! Y location of chunk
+    unsigned int Y;
+    CTile* ChunkArray[CHUNK_X_DEM][CHUNK_Y_DEM];
+    CTile* ChunkWallArray[CHUNK_Y_DEM][CHUNK_Y_DEM];
+
+
+    CChunk();
+    ~CChunk();
+
+    void OnInit();
+
+    void OnLoad(const char* File);
+
 };
 
 #endif //_CCHUNK_H_
