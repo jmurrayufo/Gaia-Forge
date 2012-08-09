99ba16df27c14d2738bed80d60ec9292b072197e
John Murray <jmurrayufo@gmail.com>
2012-08-08 23:31:12 -0600

    Added some more member varaibles to CMap, and cleaned up the header. CMap() created to spawn a bunch of NULL pointers.

--- Src/system/map/CMap.cpp
+++ Src/system/map/CMap.cpp
@@ -1,7 +1,23 @@
 #include "CMap.h"
 
 CMap::CMap() {
+    X=0;
+    Y=0;
+    for (int i = 0; i < MAP_WORLD_WIDTH; ++i)
+    {
+        for (int j = 0; j < MAP_WORTH_HEIGHT; ++j)
+        {
+            Chunks[i][j]=NULL;
+        }
+    }
 }
 
 CMap::~CMap() {
+        for (int i = 0; i < MAP_WORLD_WIDTH; ++i)
+    {
+        for (int j = 0; j < MAP_WORTH_HEIGHT; ++j)
+        {
+            delete Chunks[i][j];
+        }
+    }
 }
--- Src/system/map/CMap.h
+++ Src/system/map/CMap.h
@@ -1,6 +1,13 @@
 #ifndef _CMAP_H_
     #define _CMAP_H_
 
+#include "CChunk.h"
+
+
+#define MAP_WORLD_WIDTH    8
+#define MAP_WORTH_HEIGHT   2
+
+
 
 /*!
 \brief Maintain the map of a given world. Allow the system to talk to the chunks.
@@ -18,77 +25,92 @@ to access them.
 
 class CMap {
 
-   public:
-      CMap();
-      ~CMap();
+public:
+   //! Current location in the X direction in tiles.
+   float X;
 
-      //! Called to load a map file.
-      // TODO: Make this function
-      void OnLoad(const char* File);
+   //! Current location in the Y direction in tiles. 
+   float Y;
 
-      //! Save the map file to a given location.
+   //! Pointers to all the CChunks in this map
+   CChunk* Chunks[MAP_WORLD_WIDTH][MAP_WORTH_HEIGHT];
+
+   //! Seed that this map is based off of. Add Perlin noise should use the same seed!
+   int Seed;
+
+public:
+   CMap();
+   ~CMap();
+
+   //! Called to load a map file.
+   // TODO: Make this function
+   void OnLoad(const char* File);
+
+   //! Save the map file to a given location.
+
+   // TODO: Make this function
+   void OnSave(const char* File);
+
+   //! Save the map file to it's default location
+
+   // TODO: Make this function
+   void OnSave();
+
+   //! Called every frame. This should take care of dynamic load/free of chunks.
+
+   // TODO: Make this function
+   void OnLoop();
+
+   /*! 
+      Render the current scene. 
+      This includes Background, BackgroundTiles, Tiles, and water/lava (probably in that order).
+   */
+   // TODO: Make this function
+   void OnRenderGL(float x,float y,float w=WWIDTH,float h=WHEIGHT);
+
+   //! This map file is no longer needed, close down. 
+
+   // TODO: Make this function
+   void OnCleanup();
+
+   // Access Functions
+
+   //! Determine if the location is a collide-able block. 
+
+   // TODO: Make this function
+   bool GetCollision(int,int);
+
+   /*! Determine if the location is a collide-able block. 
+      This function can be given a float value of a location in the world, it will 
+         convert it to tile coords and use that value
+      \sa GetCollision(int,int)
+   */
    
-      // TODO: Make this function
-      void OnSave(const char* File);
+   // TODO: Make this function
+   bool GetCollision(float,float);
 
-      //! Save the map file to it's default location
+   /*! Set the current screen location as an int.
+      \param int[in] Left most corner of screen location in tile coords.
+      \param int[in] Top most corner of screen location in tiles coords.
+      \param int[in] Width of screen in pixels.
+      \param int[in] Height of screen in pixels.
+   */
    
-      // TODO: Make this function
-      void OnSave();
+   // TODO: Make this function
+   int SetLocation(int,int,int,int);
 
-      //! Called every frame. This should take care of dynamic load/free of chunks.
+   /*! Set the current screen location as a float.
+      \param float[in] Left most corner of screen location in tile coords.
+      \param float[in] Top most corner of screen location in tiles coords.
+      \param int[in] Width of screen in pixels.
+      \param int[in] Height of screen in pixels.
+   */
    
-      // TODO: Make this function
-      void OnLoop();
+   // TODO: Make this function
+   int SetLocation(float,float,int,int);
 
-      //! Render the current scene. 
-      //! This includes Background, BackgroundTiles, Tiles, and water/lava (probably in that order).
-   
-      // TODO: Make this function
-      void OnRenderGL();
+private:
 
-      //! This map file is no longer needed, close down. 
-   
-      // TODO: Make this function
-      void OnCleanup();
-
-      // Access Functions
-
-      //! Determine if the location is a collide-able block. 
-   
-      // TODO: Make this function
-      bool GetCollision(int,int);
-
-      /*! Determine if the location is a collide-able block. 
-         This function can be given a float value of a location in the world, it will 
-            convert it to tile coords and use that value
-         \sa GetCollision(int,int)
-      */
-      
-      // TODO: Make this function
-      bool GetCollision(float,float);
-
-      /*! Set the current screen location.
-         \param int[in] Left most corner of screen location in tile coords.
-         \param int[in] Top most corner of screen location in tiles coords.
-         \param int[in] Width of screen in pixels.
-         \param int[in] Height of screen in pixels.
-      */
-      
-      // TODO: Make this function
-      int SetLocation(int,int,int,int);
-
-      /*! Set the current screen location.
-         \param float[in] Left most corner of screen location in tile coords.
-         \param float[in] Top most corner of screen location in tiles coords.
-         \param int[in] Width of screen in pixels.
-         \param int[in] Height of screen in pixels.
-      */
-      
-      // TODO: Make this function
-      int SetLocation(float,float,int,int);
-
-      
 };
 
 #endif
