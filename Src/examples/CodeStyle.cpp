64662deedbfb08928780f598d604d42ac3aa7012
John Murray <jmurrayufo@gmail.com>
2012-08-09 02:11:18 -0600

    Created a new code style sheet. Lots of work to be done here.

---
+++ Src/examples/CodeStyle.cpp
@@ -0,0 +1,25 @@
+// This document should have just about every example we will need to use
+
+
+// Functions
+int foo(int x, int y)   // Spaces after the , 
+{   // This always gets it's own line!
+    int x;      // Just an int
+    int y=5;    // You can declare stuff if needed!
+    int *z;     // * on pointers go with the VARiABLE! 
+
+    while( 1 ) // Spaces around the arguments help readability 
+    {   // This always gets it's own line!
+        int food;   // This scopes to the While loop, and is removed when you leave it. 
+    }
+}
+
+// TODO: Add the following to the code style sheet
+/*
+    Comments
+    Class Variables
+    Function/Variable Names
+    Header Files
+    Defines
+    Globals
+*/
