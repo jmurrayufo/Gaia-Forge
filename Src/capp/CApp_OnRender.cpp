529431ca91759572f4e925f9cb6b1815b373bfc3
John Murray <jmurrayufo@gmail.com>
2012-08-09 00:31:18 -0600

    New test code to demonstrate the EPIC new perlin2d function I made, AMAZING!

--- Src/capp/CApp_OnRender.cpp
+++ Src/capp/CApp_OnRender.cpp
@@ -25,6 +25,7 @@ void CApp::OnRender()
     x.InterpType = NOISE_INTERP_LINEAR;
     for (int i = 0; i < WWIDTH; ++i)
     {
+        break;
         GLDebug::DrawPoint(i+0.5,x.Perlin1d(i + SDL_GetTicks()/20.f) + WHEIGHT/2);
         /*
         GLDebug::DrawPoint(i+0.5,sin(i/10.0 + SDL_GetTicks()/1000.f)*50 + WHEIGHT/2 + 0.5);
@@ -34,6 +35,27 @@ void CApp::OnRender()
                             sin(i/10.0 + SDL_GetTicks()/1000.f)*50 + WHEIGHT/3 + 0.5
                             );
         */
+    }
+    Noise g;
+    g.Period = 100;
+    g.Amplitude = 0.5;  
+    g.Octaves = 1;
+    g.Persistance = 2;
+    g.Seed = SDL_GetTicks()/1000;
+    g.InterpType = NOISE_INTERP_LINEAR;
+
+    x.Amplitude = WHEIGHT/2;  
+    float offset = SDL_GetTicks()/20.f;
+    float colors[4]={1,0,0,1};
+    for (int i = 0; i < WWIDTH/16; ++i)
+    {
+        for (int j = 0; j < WHEIGHT/16; ++j)
+        {
+            if( x.Perlin1d(i + offset ) + WHEIGHT/2 > j*16 )
+                continue;
+            colors[0]=g.Perlin2d(i*16 + offset*16,j*16)+0.5;
+            DrawBox(i*16,j*16,16,16,colors);
+        }
     }
 
 
