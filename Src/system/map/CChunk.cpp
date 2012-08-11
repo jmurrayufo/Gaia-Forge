316fbb4375c7854d61da6d2c7e8f295ffae73465
John Murray <jmurrayufo@gmail.com>
2012-08-11 01:21:30 -0600

    Added some major work into the CChunk class. We have a lot of work left to do.

--- Src/system/map/CChunk.cpp
+++ Src/system/map/CChunk.cpp
@@ -1,14 +1,15 @@
 #include "CChunk.h"
 
 CChunk::CChunk()
-{
+{   
+    return;
     for(int i = 0; i < CHUNK_X_DEM; ++i)
     {
         for(int j = 0; j < CHUNK_Y_DEM; ++i)
         {
-            ChunkArray[i][j] = 0;
+            //ChunkArray[i][j] = (CTile*)0;
         }
-    }
+    }   
 }
 
 
@@ -16,16 +17,66 @@ CChunk::~CChunk()
 {
     for(int i = 0; i < CHUNK_X_DEM; ++i)
     {
-        for(int j = 0; j < CHUNK_Y_DEM; ++i)
+        for(int j = 0; j < CHUNK_Y_DEM; ++j)
         {
-            delete ChunkArray[i][j];
+            //delete ChunkArray[i][j];
         }
-
     }
-    
 }
 
 void CChunk::OnCleanup()
 {
     // Make this clean up stuff!
+}
+void CChunk::OnRenderGL(float x, float y, int w, int h)
+{
+    /*
+        Note the complex for loop here. We want to loop through the area starting at the 
+        edge of the screen and going to the right. We stop if we hit the edge of the 
+        screen OR the edge of the chunk. 
+    */
+    std::cerr << "Entered Function" << std::endl << "    " << __FILE__ << __LINE__ << std::endl;
+    std::cerr.flush();
+
+    for (int i = 0; i < w; ++i)
+    {
+        float x_world = i + x - w/2;
+        float x_chunk = fmod(x_world,w);
+        std::cerr << "Loop(i):" << i << std::endl << "   >" << __FILE__ << __LINE__ << std::endl;
+        std::cerr << "x_w:" << x_world << std::endl;
+        std::cerr << "x_w(int):" << floor(x_world) << std::endl;
+        std::cerr << "x_c:" << x_chunk << std::endl;
+
+        if( (x_world < X * CHUNK_X_DEM) || (x_world > X * CHUNK_X_DEM + CHUNK_X_DEM) )
+        {
+            std::cerr << "SKIPED!" << std::endl;
+            continue;
+        }
+
+        for (int j = 0; j < h; ++j)
+        {
+            float y_world = j + y - CHUNK_Y_DEM/2;
+            float y_chunk = fmod(y_world,CHUNK_Y_DEM);
+            std::cerr << "  Loop(j):" << j << std::endl << "     >" << __FILE__ << __LINE__ << std::endl;
+
+            if( (x_world < Y * CHUNK_Y_DEM) || (y_world > Y * CHUNK_Y_DEM + CHUNK_Y_DEM) )
+            {
+                std::cerr << "SKIPED!" << std::endl;
+                continue;
+            }
+
+            std::cout << "IN CHUNK!" << std::endl;            
+        }
+    }
+}
+
+bool CChunk::IsInRect( float x1, float x2, float y1, float y2 )
+{
+    if(             X > x2 && 
+        X+CHUNK_X_DEM < x1 && 
+                    Y > y2 &&
+        Y+CHUNK_Y_DEM < y1)
+        return false;
+    else
+        return true;
 }
--- Src/system/map/CChunk.h
+++ Src/system/map/CChunk.h
@@ -6,6 +6,10 @@
 This class maintains a large segment of tile space in the game. It will load up, modify, 
     and save groups of tiles. 
 */
+
+#include <math.h> // We needed fmod()
+#include <iostream>
+
 #include "CTile.h"
 
 #define CHUNK_X_DEM 1024
@@ -13,25 +17,18 @@ This class maintains a large segment of tile space in the game. It will load up,
 
 class CChunk 
 {
-
-
-
-
     // TODO: Add a container for a large number of tiles. The current plan is to have 
     //      1024 x 1024 tiles in a chunk (1,048,576 tiles total). This might be too much 
     //      or not enough, we need to decide. Smaller chunks will result in more 
     //      processor overhead, but less memory footprint. 
-     
-
 public:
-    //! X location of chunk
+    //! X location of chunk in meters
     unsigned int X;
-    //! Y location of chunk
+    //! Y location of chunk in meters
     unsigned int Y;
-    CTile* ChunkArray[CHUNK_X_DEM][CHUNK_Y_DEM];
-    CTile* ChunkWallArray[CHUNK_Y_DEM][CHUNK_Y_DEM];
-
-
+    //CTile* ChunkArray[16][16];
+    //CTile* ChunkWallArray[16][16];
+    
     CChunk();
     ~CChunk();
 
@@ -40,6 +37,20 @@ public:
     void OnLoad(const char* File);
 
     void OnCleanup();
+
+    /*!
+        Render the current chunk with the given frame
+    */
+    void OnRenderGL(float x, float y, int w, int h);
+
+    /*!
+        Take a 4 element float vector and return true if it touches this CChunk
+        \param x1[in]
+        \param x2[in]
+        \param y1[in]
+        \param y2[in]
+    */
+    bool IsInRect( float x1, float x2, float y1, float y2 );
 
 };
 
