33cd16c9fcfc23d5be51745a01572a629cd28c9c
John Murray <jmurrayufo@gmail.com>
2012-08-11 01:22:45 -0600

    Main runs some test code on load, but works otherwise.

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,13 +5,21 @@
 
 #include "capp/CApp.h"
 
+#include "system/map/CChunk.h"
+
 using namespace std;
 
 //Johns Test comment
 //==============================================================================
 int main(int argc, char* argv[]) {
-    CApp theApp;
+    CChunk x;
+
+    x.X=0;
+    x.Y=0;
+
+    x.OnRenderGL(0+1/16.f,2.5,5,5);
     fprintf(stdout,"Booted main()\n   %s:%d\n",__FILE__,__LINE__);
+    CApp theApp;
     return theApp.OnExecute();
 }
 //==============================================================================
