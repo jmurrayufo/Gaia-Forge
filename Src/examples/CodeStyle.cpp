311787e053442f4e9b2e5af420ae962558798277
John Murray <jmurrayufo@gmail.com>
2012-08-12 21:27:44 -0600

    Added more comments to the code style sheet.

--- Src/examples/CodeStyle.cpp
+++ Src/examples/CodeStyle.cpp
@@ -14,7 +14,32 @@ int foo(int x, int y)   // Spaces after the ,
     }
 }
 
-// Class Header Files
+//**************************
+//*** Class Header Files ***
+//**************************
+
+// All class header files must be protected by a #ifndef. Use the _Cname_H_ style (always all caps)
+#ifndef _CFOO_H_
+    #define _CFOO_H_
+
+// Class names begin with a C, following by a descriptive name. 
+class CFoo {
+public:
+   CFoo();
+   ~CFoo();
+
+   void OnCleanup();
+
+   void OnInit();
+
+   void OnLoop();
+
+   void OnRednerGL();
+
+};
+// Mark your endif to show what it links too. 
+#endif // CFoo 
+
 
 // Class Source Files
 
