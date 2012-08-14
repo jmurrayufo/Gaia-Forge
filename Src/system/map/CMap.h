d29eced1240a9ff8f5ef6dc7b30c2ab98e7550db
John Murray <jmurrayufo@gmail.com>
2012-08-14 01:07:55 -0600

    Define uses bad choice for a global name.

--- Src/system/map/CMap.h
+++ Src/system/map/CMap.h
@@ -12,7 +12,7 @@
 #include "../noise/Noise.h"
 
 //! Units of pixels on the screen
-#define TILE_SIZE 16 
+#define CMAP_TILE_SIZE 16 
 
 /*!
 \brief Maintain the map of a given world. Allow the system to talk to the chunks.
